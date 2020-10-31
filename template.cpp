d7b72daea7e606988ca1a4a67d7075d15d0ffc62
Sameer Mann <42488833+Sameer-Mann@users.noreply.github.com>
2020-10-31 14:25:25 +0530

    #1 Added template file
    Added the template file

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long int
+using namespace std;
+template<class L, class R> ostream& operator<<(ostream &os, pair<L,R> P) {return os << "(" << P.first << "," << P.second << ")";}
+template<class T> ostream& operator<<(ostream &os, vector<T> V) {for(auto v : V) os << v << " ";return os<<"";}
+template<class T> ostream& operator<<(ostream &os, set<T> S){os << "{ ";for(auto s:S) os<<s<<" ";return os<<"}";}
+template<class T> ostream& operator<<(ostream &os, unordered_set<T> S){os << "{ ";for(auto s:S) os<<s<<" ";return os<<"}";}
+template<class T> ostream& operator<<(ostream &os, multiset<T> S){os << "{ ";for(auto s:S) os<<s<<" ";return os<<"}";}
+template<class L, class R> ostream& operator<<(ostream &os, map<L,R> M) {os << "{ ";for(auto m:M) os<<"("<<m.first<<":"<<m.second<<") ";return os<<"}";}
+template<class L, class R> ostream& operator<<(ostream &os, unordered_map<L,R> M) {os << "{ ";for(auto m:M) os<<"("<<m.fi<<":"<<m.se<<") ";return os<<"}";}
+template<typename T>
+void print(T t){cout<<(t);cout<<'\n';}
+template<typename T, typename... Args>
+void print(T t, Args... args){cout << (t) <<' ';print(args...);}
